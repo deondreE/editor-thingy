e8aaa3d87983065c6ea076077fc9b334935e70f6
deondreE <deondre.eldest.english@gmail.com>
2026-04-17 15:12:37 -0700

    fixed shell.nix build system

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} };
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
@@ -6,6 +6,8 @@ pkgs.mkShell {
     cmake
     ninja
     odin
+    glslang
+    shaderc
   ];
 
   buildInputs = with pkgs; [
@@ -26,9 +28,25 @@ pkgs.mkShell {
       ]
     }"
 
+    compile_shaders() {
+      if [ -d "./core/shaders" ]; then 
+        echo "Compiling Shaders..."
+        for file in ./shaders/*{vert,frag,comp}; do 
+          if [ -f "$file" ]; then
+            glslangValidator -V "$file" -o "$file.spv"
+            echo "Compiled: $file -> $file.spv"
+          fi
+        done
+      else
+        echo "No shaders directory found to compile."
+      fi
+    }
+
     export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
     echo "--- Vulka & SDL Environment"
     echo "SDL3 version: $(pkg-config --modversion sdl3)"
     echo "Vulkan version: $(pkg-config --modversion vulkan)"
+    echo "Shader Compiler: $(glslangValidator --version | head -n 1)"
+    echo "Tip: Run 'compile_shaders' to rebuild your SPIR-V binaries."
   '';
 }
